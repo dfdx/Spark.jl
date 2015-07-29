bb2623e926b07171dba638da8e19a9b7de8c4226
dfdx <faithlessfriend@gmail.com>
2015-07-30 02:38:15 +0300

    writing iterator from Java to Julia now works

--- src/worker.jl
+++ src/worker.jl
@@ -24,33 +24,48 @@ end
 
 writeobj(sock::TcpSocket, obj) = writeobj(sock, convert(Vector{Uint8}, obj))
 
-## function launch_worker()
-##     port = int(readline(STDIN))
-##     info("JuliaWorker: Connecting to port $(port)!")
-##     sock = connect("127.0.0.1", port)
-##     info("JuliaWorker: Connected!")
-##     part_id = readint(sock)
-##     info("JuliaWorker: partition id = $part_id")
-##     cmd = readobj(sock)
-##     info("JuliaWorker: cmd bytes = $cmd")
-##     writeobj(sock, "hello")
-##     writeobj(sock, "I'm working...")
-##     writeobj(sock, "good bye")
-##     info("JuliaWorker: Wrote object")
-##     write(sock, -1)
-##     info("JuliaWorker: Exiting")
-## end
+
+function load_stream(sock::TcpSocket)
+    function _it()
+        _next = readobj(sock)  # should it be here or in outer function?
+        while _next != END_OF_DATA_SECTION
+            produce(_next)
+            _next = readobj(sock)
+        end
+    end
+    return Task(_it)
+end
+
+
+function dump_stream(it, sock)
+    # TODO
+end
+
 
 function launch_worker()
-    try
-        port = int(readline(STDIN))
-        info("JuliaWorker: Connecting to port $(port)!")
-        sock = connect("127.0.0.1", port)
-        info("JuliaWorker: Connected!")
+    port = int(readline(STDIN))
+    info("JuliaWorker: Connecting to port $(port)!")
+    sock = connect("127.0.0.1", port)
+    try                
         part_id = readint(sock)
         info("JuliaWorker: partition id = $part_id")
+        
         cmd = readobj(sock)
-        info("JuliaWorker: cmd bytes = $cmd")
+        info("JuliaWorker: command = $cmd")
+        
+        fst = readobj(sock)
+        info("JuliaWorker: $fst")
+        
+        # we need to get iterator to apply function to it,
+        # but actually data is loaded actively (both ways)        
+        it = load_stream(sock)
+
+        info("JuliaWorker: got iterator")
+
+        for v in it
+            info("JuliaWorker: $v")
+        end
+
         writeobj(sock, "hello")
         writeobj(sock, "I'm working...")
         writeobj(sock, "good bye")
@@ -59,11 +74,10 @@ function launch_worker()
         writeint(sock, END_OF_STREAM)
         info("JuliaWorker: Exiting")
     catch e
-        # TODO: handle the case when JVM closes connection 
+        # TODO: handle the case when JVM closes connection
+        info("JuliaWorker: exception = $e")
         writeint(sock, JULIA_EXCEPTION_THROWN)
         # TODO: write stacktrace to the socket
         exit(-1)
     end
 end
-
-
