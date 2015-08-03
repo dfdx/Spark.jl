e037b092153074b9fc56d3de2dab73c5c1736a82
dfdx <faithlessfriend@gmail.com>
2015-08-04 02:05:42 +0300

    everything in its right plac

--- src/core.jl
+++ src/core.jl
@@ -39,55 +39,6 @@ end
 
 inc_it(it) = map(x -> x + 1, it)
 
-using Compat
-import Base.convert
-import JavaCall: JNIEnv, jnifunc, penv
-
-## function convert(::Type{Array{JObject, 1}}, obj::JObject)
-##     sz = ccall(jnifunc.GetArrayLength, jint,
-##                (Ptr{JNIEnv}, Ptr{Void}), penv, obj.ptr)
-##     ret = Array(JObject, sz)
-##     for i=1:sz
-##         ptr = ccall(jnifunc.GetObjectArrayElement, Ptr{Void},
-##                   (Ptr{JNIEnv}, Ptr{Void}, jint), penv, obj.ptr, i-1)
-##         ret[i] = JObject(ptr)
-##     end
-##     return ret
-## end
-
-for (x, y, z) in [ (:jboolean, :(jnifunc.GetBooleanArrayElements), :(jnifunc.ReleaseBooleanArrayElements)),
-                  (:jchar, :(jnifunc.GetCharArrayElements), :(jnifunc.ReleaseCharArrayElements)),
-                  (:jbyte, :(jnifunc.GetByteArrayElements), :(jnifunc.ReleaseByteArrayElements)),
-                  (:jshort, :(jnifunc.GetShortArrayElements), :(jnifunc.ReleaseShortArrayElements)),
-                  (:jint, :(jnifunc.GetIntArrayElements), :(jnifunc.ReleaseIntArrayElements)),
-                  (:jlong, :(jnifunc.GetLongArrayElements), :(jnifunc.ReleaseLongArrayElements)),
-                  (:jfloat, :(jnifunc.GetFloatArrayElements), :(jnifunc.ReleaseFloatArrayElements)),
-                  (:jdouble, :(jnifunc.GetDoubleArrayElements), :(jnifunc.ReleaseDoubleArrayElements)) ]
-    m=quote
-        function convert(::Type{Array{$(x),1}}, obj::JObject)
-            sz = ccall(jnifunc.GetArrayLength, jint, (Ptr{JNIEnv}, Ptr{Void}), penv, obj.ptr)    
-            arr = ccall($(y), Ptr{$(x)}, (Ptr{JNIEnv}, Ptr{Void}, Ptr{jboolean} ), penv, obj.ptr, C_NULL )
-            jl_arr::Array = pointer_to_array(arr, (@compat Int(sz)), false)
-            jl_arr = deepcopy(jl_arr)
-            ccall($(z), Void, (Ptr{JNIEnv},Ptr{Void}, Ptr{$(x)}, jint), penv, obj.ptr, arr, 0)
-            return jl_arr
-        end
-    end
-    eval(m)
-end
-
-
-function convert{T}(::Type{Array{T, 1}}, obj::JObject)
-    sz = ccall(jnifunc.GetArrayLength, jint,
-               (Ptr{JNIEnv}, Ptr{Void}), penv, obj.ptr)
-    ret = Array(T, sz)
-    for i=1:sz
-        ptr = ccall(jnifunc.GetObjectArrayElement, Ptr{Void},
-                  (Ptr{JNIEnv}, Ptr{Void}, jint), penv, obj.ptr, i-1)
-        ret[i] = convert(T, JObject(ptr))
-    end
-    return ret
-end
 
 
 
