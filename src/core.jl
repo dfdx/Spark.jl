f27fb660c45806d419393c362a96aad8129980fe
dfdx <faithlessfriend@gmail.com>
2015-08-06 03:04:03 +0300

    pipelinedrdd is (almost) pipelineable

--- src/core.jl
+++ src/core.jl
@@ -19,10 +19,6 @@ include("worker.jl")
 function demo()
     sc = SparkContext()
     java_rdd = text_file(sc, "file:///var/log/syslog")
-    rdd = PipelinedRDD(java_rdd, serialized(identity))
-    r = jcall(rdd.jrdd, "collect", JObject, ())
-    listmethods(java_rdd.jrdd, "collect")
-    listmethods(rdd.jrdd, "collect")
-    arr = convert(Array{Array{Uint8,1},1}, r)
+    rdd = PipelinedRDD(java_rdd, identity)
+    arr = collect(rdd)
 end
-
--- src/rdd.jl
+++ src/rdd.jl
@@ -5,19 +5,33 @@ type JavaRDD <: RDD
     jrdd::JJavaRDD
 end
 
+
 type PipelinedRDD <: RDD
+    parent::RDD
     jrdd::JJuliaRDD
 end
 
-function PipelinedRDD(parent::RDD, func::Array{Uint8, 1})        
-    jrdd = jcall(JJuliaRDD, "fromJavaRDD", JJuliaRDD,
-                 (JJavaRDD, Array{jbyte, 1}),
-                 parent.jrdd, convert(Array{jbyte, 1}, func))
-    PipelinedRDD(jrdd)
+
+function PipelinedRDD(parent::RDD, func::Function)
+    jrdd = if !isa(parent, PipelinedRDD)
+        command = serialized(func)
+        jrdd = jcall(JJuliaRDD, "fromJavaRDD", JJuliaRDD,
+                     (JJavaRDD, Array{jbyte, 1}),
+                     parent.jrdd, convert(Array{jbyte, 1}, command))
+        PipelinedRDD(parent, jrdd)
+    else
+        pipeline_func = (split, it) -> func(split, parent.func(split, it))
+        error("Not implemented yet")
+    end
 end
-    
+
+
+function jrdd(rdd)
+
+end
+
+
 function collect{T}(rdd::RDD, typ::Type{T}=Array{Array{jbyte,1},1})
     res = jcall(rdd.jrdd, "collect", JObject, ())
     return convert(T, res)
 end
-
