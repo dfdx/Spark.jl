cb03de35e1f9f76f18f0abee488029591c35158b
Andrei Zhabinski <a.zhabinski@adform.com>
2016-07-27 16:27:21 +0300

    fixed tests, windows classpath

--- src/init.jl
+++ src/init.jl
@@ -2,7 +2,7 @@
 function init()
     envcp = get(ENV, "CLASSPATH", "")
     sparkjlassembly = Pkg.dir("Spark", "jvm", "sparkjl", "target", "sparkjl-0.1-assembly.jar")
-    classpath = "$envcp:$sparkjlassembly"
+    classpath = @windows? "$envcp;$sparkjlassembly" : "$envcp:$sparkjlassembly"
     try
         # prevent exceptions in REPL on code reloading
         JavaCall.init(["-ea", "-Xmx1024M", "-Djava.class.path=$classpath"])
