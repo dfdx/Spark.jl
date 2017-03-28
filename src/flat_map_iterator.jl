949aceef490a889e2102d15ed9c1d2d0d0db21a6
JohnAdders <john@theadcocks.co.uk>
2017-03-28 18:55:19 +0100

    Renamed  parameter variable to be consistent with class name

--- src/flat_map_iterator.jl
+++ src/flat_map_iterator.jl
@@ -10,16 +10,16 @@ Each state tuple contains
   - the inner iterator
   - the state of the inner iterator
 """
-function get_state_for_next_value(iosi::FlatMapIterator, outer_state)
-    if !done(iosi.outer_iterator, outer_state)
-        next_outer = next(iosi.outer_iterator, outer_state)
+function get_state_for_next_value(fmi::FlatMapIterator, outer_state)
+    if !done(fmi.outer_iterator, outer_state)
+        next_outer = next(fmi.outer_iterator, outer_state)
         next_inner_state = start(next_outer[1])
         if !done(next_outer[1], next_inner_state)
             # found something
             (next_outer[2], next_outer[1], next_inner_state)
         else
             # inner is empty so move to next outer
-            get_state_for_next_value(iosi::FlatMapIterator, next_outer[2])
+            get_state_for_next_value(fmi::FlatMapIterator, next_outer[2])
         end
     else
         # we've reached the end so flag using Void
@@ -27,21 +27,21 @@ function get_state_for_next_value(iosi::FlatMapIterator, outer_state)
     end
 end
 
-function Base.start(iosi::FlatMapIterator)
-    get_state_for_next_value(iosi, start(iosi.outer_iterator))
+function Base.start(fmi::FlatMapIterator)
+    get_state_for_next_value(fmi, start(fmi.outer_iterator))
 end
 
-function Base.next(iosi::FlatMapIterator, state)
+function Base.next(fmi::FlatMapIterator, state)
     next_inner = next(state[2], state[3])
     if !done(state[2], next_inner[2])
         # move onto next value in inner iterator
         (next_inner[1], (state[1], state[2], next_inner[2]))
     else
         # move onto next value using outer iterator
-        (next_inner[1], get_state_for_next_value(iosi, state[1]))
+        (next_inner[1], get_state_for_next_value(fmi, state[1]))
     end
 end
 
-function Base.done(iosi::FlatMapIterator, state)
+function Base.done(fmi::FlatMapIterator, state)
     state[2] == Void
 end
