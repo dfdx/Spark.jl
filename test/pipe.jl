5543373b9af26efe81de7ae7ecc19a8f1bb65e22
John Adcock <adcockj@users.sourceforge.net>
2017-04-15 15:50:02 +0100

    Fix windows version of shell script pipe command

--- test/pipe.jl
+++ test/pipe.jl
@@ -1,12 +1,13 @@
 # test pipe
 
-const PIPETEST = joinpath(dirname(@__FILE__), "pipetest.sh")
 const JULIA = joinpath(JULIA_HOME, "julia")
 
 @static if is_windows()
+const PIPETEST = joinpath(dirname(@__FILE__), "pipetest.cmd")
 const TESTJL = "\"while !eof(STDIN); print(\\\"Julia\\\", readline(STDIN)); end\""
 const TESTJLENV = "\"while !eof(STDIN); print(ENV[\\\"HEADER\\\"], readline(STDIN)); end\""
 else
+const PIPETEST = joinpath(dirname(@__FILE__), "pipetest.sh")
 const TESTJL = """while !eof(STDIN); print("Julia", readline(STDIN)); end"""
 const TESTJLENV = """while !eof(STDIN); print(ENV["HEADER"], readline(STDIN)); end"""
 end
@@ -15,10 +16,8 @@ sc = SparkContext(master="local")
 
 nums1 = parallelize(sc, 1:30)
 
-@static if is_unix()
 nums11 = collect(pipe(nums1, PIPETEST))
 @test length(nums11) > 30
-end
 
 nums11 = collect(pipe(nums1, [JULIA, "-e", TESTJL]))
 @test length(nums11) == 30
@@ -34,10 +33,8 @@ end
 
 pnums1 = cartesian(nums1, nums1)
 
-@static if is_unix()
 pnums11 = collect(pipe(pnums1, PIPETEST))
-@test length(collect(pipe(pnums1, PIPETEST))) > 900
-end
+@test length(pnums11) > 900
 
 pnums11 = collect(pipe(pnums1, [JULIA, "-e", TESTJL]))
 @test length(pnums11) == 900
