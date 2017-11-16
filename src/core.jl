531d63174e416f18874bc8efc091e5a8697059cc
dfdx <faithlessfriend@gmail.com>
2017-11-17 01:40:20 +0300

    basic implementations for join() and group_by() for datasets

--- src/core.jl
+++ src/core.jl
@@ -14,7 +14,10 @@ const JSparkSessionBuilder = @jimport org.apache.spark.sql.SparkSession$Builder
 const JDataFrameReader = @jimport org.apache.spark.sql.DataFrameReader
 const JDataFrameWriter = @jimport org.apache.spark.sql.DataFrameWriter
 const JDataset = @jimport org.apache.spark.sql.Dataset
+const JRelationalGroupedDataset = @jimport org.apache.spark.sql.RelationalGroupedDataset
 const JGenericRow = @jimport org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+const JRowFactory = @jimport org.apache.spark.sql.RowFactory
+const JRow = @jimport org.apache.spark.sql.Row
 const JColumn = @jimport org.apache.spark.sql.Column
 const JSQLFunctions = @jimport org.apache.spark.sql.functions
 # RDD
