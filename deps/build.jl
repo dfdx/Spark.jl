218d4520d170080c74ec161e526bbb3f6c8eca59
Andrei Zhabinski <faithlessfriend@gmail.com>
2018-02-28 23:24:25 +0300

    configurable spark version (#56)

--- deps/build.jl
+++ deps/build.jl
@@ -7,6 +7,8 @@ catch
     error("Cannot find maven. Is it installed?")
 end
 
+SPARK_VERSION = get(ENV, "BUILD_SPARK_VERSION", "2.1.0")
+
 cd("../jvm/sparkjl")
-run(`$mvn clean package`)
+run(`$mvn clean package -Dspark.version=$SPARK_VERSION`)
 
