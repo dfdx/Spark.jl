0ae53c0c48be8f8328338234fc35c7ccd2495a34
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2018-10-10 00:14:25 +0300

    build on Windows

--- deps/build.jl
+++ deps/build.jl
@@ -9,6 +9,23 @@ end
 
 SPARK_VERSION = get(ENV, "BUILD_SPARK_VERSION", "2.1.0")
 
-cd("../jvm/sparkjl")
-run(`$mvn clean package -Dspark.version=$SPARK_VERSION`)
+cd(joinpath(dirname(@__DIR__), "jvm/sparkjl")) do
+    run(`$mvn clean package -Dspark.version=$SPARK_VERSION`)
+end
 
+if Sys.iswindows()   
+    # on Windows Spark requires HADOOP_HOME variable to be defined and contain file `bin/winutils.exe`
+    # here we create such a file in deps/hadoop/bin
+    if !haskey(ENV, "HADOOP_HOME")
+        HADOOP_COMMONS_URL = ("https://github.com/steveloughran/winutils/blob/master/" *
+                              "hadoop-2.7.1/bin/winutils.exe")
+        hadoop_bin_path = joinpath(@__DIR__, "hadoop", "bin")
+        
+        if !isdir(hadoop_bin_path)
+            mkpath(hadoop_bin_path)
+        end
+        download(HADOOP_COMMONS_URL, joinpath(hadoop_bin_path, "winutils.exe"))
+        ENV["HADOOP_HOME"] = dirname(hadoop_bin_path)
+        
+    end
+end
