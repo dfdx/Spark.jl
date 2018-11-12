2613149494cd8ddf715b0e3fcbef35f78700d04b
Avik Sengupta <avik@sengupta.net>
2018-11-12 14:59:48 +0000

    Update Azure install script for 1.0

--- examples/InstallJuliaHDI.sh
+++ examples/InstallJuliaHDI.sh
@@ -4,8 +4,27 @@
 # This script, or a derivative should be set as a script action when deploying an HDInsight cluster
 
 # install julia v0.6
-curl -s https://julialang-s3.julialang.org/bin/linux/x64/0.6/julia-0.6.0-linux-x86_64.tar.gz | sudo tar -xz -C /usr/local/
-JULIA_HOME=/usr/local/julia-903644385b/bin
+curl -s https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.2-linux-x86_64.tar.gz | sudo tar -xz -C /usr/local/
+JULIA_HOME=/usr/local/julia-1.0.2/bin
+
+#Patch Julia for #28815
+cat <<EOT >> $JULIA_HOME/../etc/julia/startup.jl
+function Base.touch(path::AbstractString)
+    f = Base.Filesystem.open(path, Base.JL_O_WRONLY | Base.JL_O_CREAT, 0o0666)
+    try
+        if Sys.isunix()
+            ret = ccall(:futimes, Cint, (Cint, Ptr{Cvoid}), fd(f), C_NULL)
+            systemerror(:futimes, ret != 0, extrainfo=path)
+        else
+            t = time()
+            futime(f,t,t)
+        end
+    finally
+        close(f)
+    end
+    path
+end
+EOT
 
 # install maven
 curl -s http://mirror.olnevhost.net/pub/apache/maven/binaries/apache-maven-3.2.2-bin.tar.gz | sudo tar -xz -C /usr/local/
@@ -13,17 +32,20 @@ export M2_HOME=/usr/local/apache-maven-3.2.2
 export PATH=$M2_HOME/bin:$PATH
 
 # Create Directories
-export JULIA_PKGDIR="/home/hadoop/.julia/"
-mkdir -p ${JULIA_PKGDIR}
+export JULIA_DEPOT_PATH="/home/hadoop/.julia/"
+mkdir -p ${JULIA_DEPOT_PATH}
 
 # Set Environment variables for current session
 export PATH=${PATH}:${MVN_HOME}/bin:${JULIA_HOME}/bin
 export HOME="/root"
-echo "Installing Julia Packages in Julia Folder ${JULIA_PKGDIR}"
+echo "Installing Julia Packages in Julia Folder ${JULIA_DEPOT_PATH}"
 #Install Spark.jl
-$JULIA_HOME/julia -e 'Pkg.add("Spark");Pkg.checkout("Spark");Pkg.build("Spark"); using Spark;'
+$JULIA_HOME/julia -e 'using Pkg; Pkg.add("Spark");Pkg.build("Spark"); using Spark;'
 declare -a users=("spark" "yarn" "hadoop" "sshuser") #TODO: change accordingly
 SPARK_HOME=/usr/hdp/current/spark2-client
+echo "spark.executorEnv.JULIA_HOME ${JULIA_HOME}" >> ${SPARK_HOME}/conf/spark-defaults.conf
+echo "spark.executorEnv.JULIA_DEPOT_PATH ${JULIA_DEPOT_PATH}" >> ${SPARK_HOME}/conf/spark-defaults.conf
+echo "spark.executorEnv.JULIA_VERSION v1.0.2" >> ${SPARK_HOME}/conf/spark-defaults.conf
 for cusr in "${users[@]}"; do
    echo " Adding vars for ser ${cusr}"
    echo "" >> /home/${cusr}/.bashrc
@@ -31,11 +53,8 @@ for cusr in "${users[@]}"; do
    echo "export PATH=${PATH}:${MVN_HOME}/bin:${JULIA_HOME}" >> /home/${cusr}/.bashrc   
    echo "export YARN_CONF_DIR=/etc/hadoop/conf" >> /home/${cusr}/.bashrc
    echo "export JULIA_HOME=${JULIA_HOME}" >> /home/${cusr}/.bashrc
-   echo "export JULIA_PKGDIR=${JULIA_PKGDIR}" >> /home/${cusr}/.bashrc   
+   echo "export JULIA_DEPOT_PATH=${JULIA_DEPOT_PATH}" >> /home/${cusr}/.bashrc   
    echo "source ${SPARK_HOME}/bin/load-spark-env.sh" >> /home/${cusr}/.bashrc 
-   echo "spark.executorEnv.JULIA_HOME ${JULIA_HOME}" >> ${SPARK_HOME}/conf/spark-defaults.conf
-   echo "spark.executorEnv.JULIA_PKGDIR ${JULIA_PKGDIR}" >> ${SPARK_HOME}/conf/spark-defaults.conf
-   echo "spark.executorEnv.JULIA_VERSION v0.6" >> ${SPARK_HOME}/conf/spark-defaults.conf
    # Set Package folder permissions   
-   setfacl -R -m u:${cusr}:rwx ${JULIA_PKGDIR};  
+   setfacl -R -m u:${cusr}:rwx ${JULIA_DEPOT_PATH};  
 done
