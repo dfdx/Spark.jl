5c3579b26ea063217899074abc4dee3979427451
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2018-11-25 03:26:00 +0300

    fixed (most of the) tests for julian versions of Spark RDD methods

--- test/julian_versions.jl
+++ test/julian_versions.jl
@@ -1,30 +1,50 @@
 @testset "julian_versions" begin
 
-# test filter
-sc = SparkContext(master="local")
+    # test filter
+    sc = SparkContext(master="local")
 
-rdd = parallelize(sc, 1:30)
+    rdd = parallelize(sc, 1:30)
+    pairs = map_pair(rdd, x -> (x,1))
 
-f = (partition, idx) -> sum(partition)
-@test map_partitions_with_index(rdd, f) |> collect == map_partitions_with_index(f, rdd) |> collect
-    @test map_partitions(rdd, partition -> sum(partition)) == map_partitions(partition -> sum(partition), rdd)
-    @test map_partitions_pair(rdd, partition -> (sum(partition), 1)) == map_partitions_pair(rdd, partition -> (sum(partition), 1))
-@test map_pair(rdd, x -> (x,1)) == map_pair(x -> (x,1), rdd)
-    
-    
-# map_partitions_with_index(f::Function, rdd::RDD) = map_partition_with_index(rdd, f)
-# map_partitions(f::Function, rdd::RDD) = map_partitions(rdd, f)
-# map_partitions_pair(f::Function, rdd::RDD) = map_partitions_pair(rdd, f)
-# Base.map(f::Function, rdd::RDD) = map(rdd, f)
-# map_pair(f::Function, rdd::RDD) = map_pair(rdd, f)
-# flat_map(f::Function, rdd::RDD) = flat_map(rdd, f)
-# flat_map_pair(f::Function, rdd::RDD) = flat_map_pair(f, rdd)
-# Base.filter(f::Function, rdd::SingleRDD) = filter(rdd, f)
-# Base.filter(f::Function, rdd::PairRDD) = filter(rdd, f)
-# Base.reduce(f::Function, rdd::RDD) = reduce(rdd, f)
-#     reduce_by_key(f::Function, rdd::RDD) = reduce_by_key(rdd, f)
+    f = (prt, idx) -> sum(prt)
+    @test (map_partitions_with_index(rdd, f) |> collect ==
+           map_partitions_with_index(f, rdd) |> collect)
 
-    
-close(sc)
+    f = prt -> sum(prt)
+    @test (map_partitions(rdd, f) |> collect ==
+           map_partitions(f, rdd) |> collect)
+
+    f = prt -> ((x, 1) for x in prt)
+    @test (map_partitions_pair(rdd, f) |> collect ==
+           map_partitions_pair(f, rdd) |> collect)
+
+    f = x -> x + 1
+    @test map(rdd, f) |> collect == map(f, rdd) |> collect
+
+    f = x -> (x,1)
+    @test map_pair(rdd, f) |> collect == map_pair(f, rdd) |> collect
+
+    f = x -> [x, x]
+    @test flat_map(rdd, f) |> collect == flat_map(f, rdd) |> collect
+
+    # currently broken
+    # f -> x -> [x, x]
+    # @test flat_map_pair(rdd, f) |> collect == flat_map_pair(f, rdd) |> collect
+
+    f = x -> mod(x, 2) == 1
+    @test filter(rdd, f) |> collect == filter(f, rdd) |> collect
+
+    # currently broken
+    # f = (x, y) -> mod(x, 2) == 1
+    # @test filter(pairs, f) |> collect == filter(f, pairs) |> collect
+
+    f = (+)
+    @test reduce(rdd, f) |> collect == reduce(f, rdd) |> collect
+
+    pairs2 = map_pair(parallelize(sc, ["a", "b", "a", "c"]), x -> (x, 1))
+    f = (+)
+    @test reduce_by_key(pairs2, f) |> collect == reduce_by_key(f, pairs2) |> collect
+
+    close(sc)
 
 end
