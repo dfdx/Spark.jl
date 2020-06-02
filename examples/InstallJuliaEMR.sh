a1f72394e32bc918fa9d6816e49af30257320eec
Avik Sengupta <avik@sengupta.net>
2020-06-02 10:48:25 +0100

    Allow redirects in julia download for cloud scripts.
    Upgrade to latest LTS julia release.

--- examples/InstallJuliaEMR.sh
+++ examples/InstallJuliaEMR.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # install julia
-curl -s https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-0.5.1-linux-x86_64.tar.gz | sudo tar -xz -C /usr/local/
-JULIA_DIR=/usr/local/julia-6445c82d00
+curl -sL https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.5-linux-x86_64.tar.gz | sudo tar -xz -C /usr/local/
+JULIA_DIR=/usr/local/julia-1.0.5
+JULIA_HOME=/usr/local/julia-1.0.5/bin
 
 # install maven
 curl -s http://mirror.olnevhost.net/pub/apache/maven/binaries/apache-maven-3.2.2-bin.tar.gz | sudo tar -xz -C /usr/local/
@@ -18,11 +19,11 @@ for usr in "${users[@]}"; do
    sudo echo "export SPARK_HOME=/usr/lib/spark/" >> ${ENV_FILE}
    sudo echo "export HADOOP_CONF_DIR=/etc/hadoop/conf" >> ${ENV_FILE}
    sudo echo "export YARN_CONF_DIR=/etc/hadoop/conf" >> ${ENV_FILE}
-   sudo echo "export PATH=${PATH}:${MAVEN_DIR}/bin:${JULIA_DIR}/bin" >> ${ENV_FILE} 
+   sudo echo "export PATH=${PATH}:${MAVEN_DIR}/bin:${JULIA_DIR}/bin" >> ${ENV_FILE}
    sudo echo "export SPARK_CONF_DIR=/etc/spark/conf.dist/" >> ${ENV_FILE}
    sudo echo "spark.executorEnv.JULIA_HOME ${JULIA_DIR}/bin" >> ${SPARK_CONF_DIR}/spark-defaults.conf
    sudo echo "spark.executorEnv.JULIA_PKGDIR ${JULIA_PKGDIR}" >> ${SPARK_CONF_DIR}/spark-defaults.conf
-   sudo echo "spark.executorEnv.JULIA_VERSION v0.6" >> ${SPARK_CONF_DIR}/spark-defaults.conf
+   sudo echo "spark.executorEnv.JULIA_VERSION v1.0.5" >> ${SPARK_CONF_DIR}/spark-defaults.conf
 done
 
 export SPARKJL_PROFILE=yarn
--- examples/InstallJuliaHDI.sh
+++ examples/InstallJuliaHDI.sh
@@ -4,27 +4,8 @@
 # This script, or a derivative should be set as a script action when deploying an HDInsight cluster
 
 # install julia v0.6
-curl -s https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.2-linux-x86_64.tar.gz | sudo tar -xz -C /usr/local/
-JULIA_HOME=/usr/local/julia-1.0.2/bin
-
-#Patch Julia for #28815
-cat <<EOT >> $JULIA_HOME/../etc/julia/startup.jl
-function Base.touch(path::AbstractString)
-    f = Base.Filesystem.open(path, Base.JL_O_WRONLY | Base.JL_O_CREAT, 0o0666)
-    try
-        if Sys.isunix()
-            ret = ccall(:futimes, Cint, (Cint, Ptr{Cvoid}), fd(f), C_NULL)
-            systemerror(:futimes, ret != 0, extrainfo=path)
-        else
-            t = time()
-            futime(f,t,t)
-        end
-    finally
-        close(f)
-    end
-    path
-end
-EOT
+curl -sL https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.5-linux-x86_64.tar.gz | sudo tar -xz -C /usr/local/
+JULIA_HOME=/usr/local/julia-1.0.5/bin
 
 # install maven
 curl -s http://mirror.olnevhost.net/pub/apache/maven/binaries/apache-maven-3.2.2-bin.tar.gz | sudo tar -xz -C /usr/local/
@@ -45,16 +26,16 @@ declare -a users=("spark" "yarn" "hadoop" "sshuser") #TODO: change accordingly
 SPARK_HOME=/usr/hdp/current/spark2-client
 echo "spark.executorEnv.JULIA_HOME ${JULIA_HOME}" >> ${SPARK_HOME}/conf/spark-defaults.conf
 echo "spark.executorEnv.JULIA_DEPOT_PATH ${JULIA_DEPOT_PATH}" >> ${SPARK_HOME}/conf/spark-defaults.conf
-echo "spark.executorEnv.JULIA_VERSION v1.0.2" >> ${SPARK_HOME}/conf/spark-defaults.conf
+echo "spark.executorEnv.JULIA_VERSION v1.0.5" >> ${SPARK_HOME}/conf/spark-defaults.conf
 for cusr in "${users[@]}"; do
    echo " Adding vars for ser ${cusr}"
    echo "" >> /home/${cusr}/.bashrc
    echo "export MVN_HOME=/usr/local/apache-maven-3.2.2" >> /home/${cusr}/.bashrc
-   echo "export PATH=${PATH}:${MVN_HOME}/bin:${JULIA_HOME}" >> /home/${cusr}/.bashrc   
+   echo "export PATH=${PATH}:${MVN_HOME}/bin:${JULIA_HOME}" >> /home/${cusr}/.bashrc
    echo "export YARN_CONF_DIR=/etc/hadoop/conf" >> /home/${cusr}/.bashrc
    echo "export JULIA_HOME=${JULIA_HOME}" >> /home/${cusr}/.bashrc
-   echo "export JULIA_DEPOT_PATH=${JULIA_DEPOT_PATH}" >> /home/${cusr}/.bashrc   
-   echo "source ${SPARK_HOME}/bin/load-spark-env.sh" >> /home/${cusr}/.bashrc 
-   # Set Package folder permissions   
-   setfacl -R -m u:${cusr}:rwx ${JULIA_DEPOT_PATH};  
+   echo "export JULIA_DEPOT_PATH=${JULIA_DEPOT_PATH}" >> /home/${cusr}/.bashrc
+   echo "source ${SPARK_HOME}/bin/load-spark-env.sh" >> /home/${cusr}/.bashrc
+   # Set Package folder permissions
+   setfacl -R -m u:${cusr}:rwx ${JULIA_DEPOT_PATH};
 done
