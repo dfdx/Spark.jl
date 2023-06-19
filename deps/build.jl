19f4ebff4d6bb00c58f22636665f80720a9ebd6b
amartinsson-thea <137051324+amartinsson-thea@users.noreply.github.com>
2023-06-19 11:04:56 +0200

    Update build.jl with SCALA_BINARY

--- deps/build.jl
+++ deps/build.jl
@@ -9,7 +9,7 @@ end
 
 SPARK_VERSION = get(ENV, "BUILD_SPARK_VERSION", "3.2.1")
 SCALA_VERSION = get(ENV, "BUILD_SCALA_VERSION", "2.13")
-SCALA_BINARY_VERSION = get(ENV, "BUILD_SCALA_VERSION", "2.13.6")
+SCALA_BINARY_VERSION = get(ENV, "BUILD_SCALA_BINARY_VERSION", "2.13.6")
 
 cd(joinpath(dirname(@__DIR__), "jvm/sparkjl")) do
     run(`$mvn clean package -Dspark.version=$SPARK_VERSION -Dscala.version=$SCALA_VERSION -Dscala.binary.version=$SCALA_BINARY_VERSION`)
